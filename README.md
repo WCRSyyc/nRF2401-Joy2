195a885b4b0a2f764a821aa47d9963b4399cd51f
H. Phil Duby <philduby@phriendly.net>
2019-01-01 18:25:46 -0700

    Refactor and cleanup leftover example code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # nRF2401-Joy2
 
-This sketch is used to test joystick and nRF24L01 radio module protoshields for the [carbot](https://github.com/WCRSyyc/carbot).  It reads inputs from the joystick, populates data packets, and sends them to the radio.  Another sketch and radio are needed to display the packet contents.
+This sketch is used to test joystick and nRF24L01 radio module protoshields for the [carbot](https://github.com/WCRSyyc/carbot).  It reads inputs from the joystick, populates data packets, and sends them to the radio.  Another sketch and radio are needed to display the packet contents. [nRF2401-motors](https://github.com/WCRSyyc/nRF2401-motors.git) will work, with or without having a motor shield and motors.
 
 This code demonstrates using a struct for the packet buffer.  The fields can be populated directly, instead of needing to append each to end of the buffer array.  This simplifies some of the code, as well as providing a little code level documentation about what is in the buffer.
--- nRF2401-Joy2.ino
+++ nRF2401-Joy2.ino
@@ -1,109 +1,141 @@
 /*
-* Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
-* Updated: Dec 2014 by TMRh20
+* Send analog readings in RF24 radio payload.
+*
+* Test and debug sketch for joystick protoshield
+*
+* Initially based on the GettingStarted RF24 library example.
+*
+* Use nRF2401-motors sketch, or similar to receive and report sent data
+* https://github.com/WCRSyyc/nRF2401-motors.git
 */
 
 #include <SPI.h>
 #include "RF24.h"
 
-struct txMessStru{
+// Specify which pins the joystick potentiometers and button are attached to
+const int FIRST_A_IN_SOURCE = A0;
+const int SECOND_A_IN_SOURCE = A1;
+const int BUTTON_SOURCE = 16;
+const unsigned long PACKET_DELAY = 50;  // milliseconds (0.05 seconds)
+const byte pipeAddress[] = "1Node";  // Must be the same as opened by receiver
+// Lower values give faster response
+const int MAX_SENSOR = 1023;  // 10 bit ADC
+const int MAX_MAP_ANALOG = 255;  // standard PWM analogWrite maximum
+const int MIN_MAP_ANALOG = -255;  // negative for backwards
+
+// Raw sensor
+struct joystickSensor {
+  int x, y;
+  int button;
+};
+
+// Data Packet (payload)
+struct appPacket {
   int buffType;
   int x, y, z;
 };
-const unsigned long PACKET_DELAY = 50; // milliseconds (0.05 seconds)
-// Lower values give faster response
-
-txMessStru myMessage;
-int ptr;                    // index into message buffer
-char workBuff[ 6 ];         // ascii to char work buffer
-
-const int analogInPinA0 = A0;  // Analog input pin that the potentiometer is attached to
-const int analogInPinA1 = A1;  // Analog input pin that the potentiometer is attached to
-const int buttonPin = 16;
-
-int sensorValueX = 0;        // value read from the pot
-int sensorValueY = 0;        // value read from the pot
-int sensorValueZ = 0;        // value read from the button
-//int x, y, z;                 // mapped values for motors
-
-
-/****************** User Config ***************************/
-/***      Set this radio as radio number 0 or 1         ***/
-bool radioNumber = 0;
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
 RF24 radio( 7, 8 );
-/**********************************************************/
 
-byte addresses[][ 6 ] = { "1Node", "2Node" };
 
 void setup() {
   Serial.begin( 115200 );
-  pinMode( 16, INPUT_PULLUP);
+  Serial.print(F("Joystick Test Starting up"));
+
+  pinMode( BUTTON_SOURCE, INPUT_PULLUP);  // Use internal pullup so pin does not `float`
   radio.begin();
 
   // Set the PA Level low to prevent power supply related issues since this is a
-  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-
+  // test and debugging sketch, and the likelihood of close proximity of the
+  // devices. RF24_PA_MAX is default.
   radio.setPALevel(RF24_PA_LOW);
 
-  Serial.print("Joystick Starting up as radio ");
-  Serial.println ( radioNumber );
-
-  // Open a pipe to write to the radio
-  radio.openWritingPipe(addresses[0]);
+  // Open a pipe to send packets over the radio
+  radio.openWritingPipe(pipeAddress);
   radio.stopListening();
 }
 
+
 void loop() {
-  getSensorData();
-   //buildPacket();
+  joystickSensor joystick;
+  appPacket rfMessage;
 
-  // Serial.println(myMessage.messBuff);
+  joystick = readJoystick();
+  reportRawJoystick(joystick);  // Testing / debug
+  rfMessage = joystickToPacket(joystick);
+  reportDataPacket(rfMessage);  // Testing / debug
 
-  if (!radio.write(&myMessage, sizeof(txMessStru))){
-    Serial.println("tx error");
+  if(!radio.write(&rfMessage, sizeof(appPacket))) {
+    Serial.println(F("tx error"));  // No ack seen
   }
 
-  delay( PACKET_DELAY );
-}
-
-void getSensorData()
-{
-  sensorValueX = analogRead( analogInPinA0 );
-  sensorValueY = analogRead( analogInPinA1 );
-  sensorValueZ = digitalRead( 16 );
-
-  myMessage.x = map( sensorValueX, 0, 1023, -255, 255 );
-  myMessage.y = map( sensorValueY, 0, 1023, 255, -255 );
-  myMessage.z = map( sensorValueZ, 0, 1, 1, 0 );
-//  Serial.print ( "Packet: x=" );
-//  Serial.print ( myMessage.x );
-//  Serial.print ( ", y=" );
-//  Serial.print ( myMessage.y );
-//  Serial.print ( ", z=" );
-//  Serial.println ( myMessage.z );
-} // ./getSensorData()
-
-/*
-void buildPacket()
-{
-  ptr = 0;
-  buffAppend( x );
-  buffAppend( y );
-  buffAppend( z );
-  myMessage.messBuff[ ptr - 1 ] = 0;// clear trailing comma
+  delay(PACKET_DELAY);  // Wait awhile before sending next packet
 }
 
 
-void buffAppend( const int value )
+/**
+ * readJoystick
+ *
+ * @returns current joystick settings
+ */
+joystickSensor readJoystick()
 {
-  itoa( value, workBuff, 10);
-  strncpy( &myMessage.messBuff[ ptr ], workBuff, strlen( workBuff ));
-  ptr += strlen( workBuff );
-  myMessage.messBuff[ ptr ] = ',';
-  ptr++;
+  joystickSensor j;  // Local joystick sensor data
+  j.x = analogRead(FIRST_A_IN_SOURCE);
+  j.y = analogRead(SECOND_A_IN_SOURCE);
+  j.button = digitalRead(BUTTON_SOURCE);
+  return j;
+} // ./ readJoystick()
+
+
+/**
+ * joystickToPacket
+ * Convert joystick measurements to data packet for radio
+ *
+ * @params jStick joystick measurement values
+ * @returns applicate data packet
+ */
+appPacket joystickToPacket(joystickSensor jStick) {
+  appPacket data;
+
+  // Convert raw sensor values to control range: natural and inverted
+  data.x = map( jStick.x, 0, MAX_SENSOR, MIN_MAP_ANALOG, MAX_MAP_ANALOG );
+  data.y = map( jStick.y, 0, MAX_SENSOR, MAX_MAP_ANALOG, MIN_MAP_ANALOG );
+  data.z = map( jStick.button, 0, 1, 1, 0 );  // inverted
+  return data;
 }
 
-*/
+
+/**
+ * reportRawJoystick
+ * send the raw joystick readings to the serial port
+ *
+ * @param joystick struct containing joystick measurements
+ */
+void reportRawJoystick(joystickSensor jStick) {
+  Serial.print(F("Raw Joystick measurements"));
+  Serial.print(F(": x="));
+  Serial.print(jStick.x);
+  Serial.print(F("; y="));
+  Serial.print(jStick.y);
+  Serial.print(F("; button="));
+  Serial.println(jStick.button);
+}  // ./reportRawJoystick()
+
+
+/**
+ * reportDataPacket
+ * report the generated data packet details
+ *
+ * @param packet struct containing application data packet
+ */
+void reportDataPacket(appPacket packet) {
+  Serial.print(F( "Generated application Data Packet"));
+  Serial.print(F(": x="));
+  Serial.print(packet.x);
+  Serial.print(F("; y="));
+  Serial.print(packet.y);
+  Serial.print(F("; z="));
+  Serial.println(packet.z);
+}  // ./reportDataPacket()
